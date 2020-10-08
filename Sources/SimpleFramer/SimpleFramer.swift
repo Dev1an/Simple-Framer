62ef9cc28f72b9f7f6939f2f3ff42c71f1ed7e4f
Damiaan Dufaux <damiaan@dpro.be>
2020-10-09 01:00:41 +0200

    Add chunk test

--- Sources/SimpleFramer/SimpleFramer.swift
+++ Sources/SimpleFramer/SimpleFramer.swift
@@ -1,3 +1,57 @@
-struct SimpleFramer {
-    var text = "Hello, World!"
+import Network
+
+class PipeFramer: NWProtocolFramerImplementation {
+	static let label = "Pipe framer"
+	static let definition = NWProtocolFramer.Definition(implementation: PipeFramer.self)
+	static let delimiter = Character("|").asciiValue!
+
+	var minLengthUntilNextMessage = 1 {
+		didSet { print("👨‍💻 client:\t\tminLength set to", minLengthUntilNextMessage) }
+	}
+
+	required init(framer: NWProtocolFramer.Instance) {}
+
+	func start(framer: NWProtocolFramer.Instance) -> NWProtocolFramer.StartResult { .ready }
+
+	func handleInput(framer: NWProtocolFramer.Instance) -> Int {
+		print("👨‍💻 client: handle input")
+		while true {
+			var delimiterPosition: Int?
+			_ = framer.parseInput(minimumIncompleteLength: minLengthUntilNextMessage, maximumLength: 65535) { buffer, endOfMessage in
+				if let buffer = buffer {
+					print("👨‍💻 client:\t\tparsing buffer: \"\(String(bytes: buffer, encoding: .utf8) ?? buffer.debugDescription)\"")
+					if let indexOfDelimiter = buffer.firstIndex(of: Self.delimiter) {
+						minLengthUntilNextMessage = 1
+						delimiterPosition = indexOfDelimiter
+					} else {
+						minLengthUntilNextMessage = buffer.count + 1
+					}
+				} else {
+					print("👨‍💻 client:\t\tno buffer")
+				}
+				return 0
+			}
+
+			if let length = delimiterPosition {
+				guard framer.deliverInputNoCopy(length: length, message: .init(instance: framer), isComplete: true) else {
+					return 0
+				}
+				_ = framer.parseInput(minimumIncompleteLength: 1, maximumLength: 65535) { _,_ in 1 }
+			} else {
+				return minLengthUntilNextMessage
+			}
+		}
+	}
+
+	func handleOutput(framer: NWProtocolFramer.Instance, message: NWProtocolFramer.Message, messageLength: Int, isComplete: Bool) {
+		try! framer.writeOutputNoCopy(length: messageLength)
+		framer.writeOutput(data: [Self.delimiter])
+	}
+
+	func wakeup(framer: NWProtocolFramer.Instance) {}
+
+	func stop(framer: NWProtocolFramer.Instance) -> Bool { return true }
+
+	func cleanup(framer: NWProtocolFramer.Instance) { }
 }
+
--- Tests/SimpleFramerTests/SimpleFramerTests.swift
+++ Tests/SimpleFramerTests/SimpleFramerTests.swift
@@ -1,15 +1,127 @@
 import XCTest
 @testable import SimpleFramer
 
+import Network
+
+let clientQueue = DispatchQueue(label: "Server")
+let serverQueue = DispatchQueue(label: "Client")
+
 final class SimpleFramerTests: XCTestCase {
-    func testExample() {
+
+    func testChunkedInTheMiddle() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SimpleFramer().text, "Hello, World!")
+		var firstMessages = Set(messages[0...2])
+		let splitMessage = messages[3]
+		let lastMessage = messages.last!
+
+		let receiveFirstThreeMessages = XCTestExpectation(description: "Receive first three messages")
+		let receiveSplitMessage = XCTestExpectation(description: "Receive split message")
+		let receiveLastMessage = XCTestExpectation(description: "Receive last message")
+
+		let server = try! NWListener(using: .tcp)
+		server.newConnectionHandler = sendChunks
+
+		server.stateUpdateHandler = { state in
+			print("🖲 server:", state)
+			if state == .ready, let port = server.port {
+				print("🖲 server: listening on", port)
+			}
+		}
+
+		server.start(queue: serverQueue)
+
+		let networkParameters = NWParameters.tcp
+		networkParameters
+			.defaultProtocolStack
+			.applicationProtocols
+			.insert(
+				NWProtocolFramer.Options(definition: PipeFramer.definition),
+				at: 0
+			)
+		let client = NWConnection(to: .hostPort(host: "localhost", port: server.port!), using: networkParameters)
+
+		func receiveNext() {
+			client.receiveMessage { (data, context, complete, error) in
+				let content: String
+				if let data = data {
+					if let string = String(data: data, encoding: .utf8) {
+						content = string
+						firstMessages.remove(content)
+					} else {
+						content = data.description
+					}
+				} else {
+					content = "<no data>"
+					if error == nil && complete {
+						firstMessages.remove("")
+					}
+				}
+				print("👨‍💻 client: received \"\(content)\"", context.debugDescription, complete, error?.localizedDescription ?? "No error")
+				if firstMessages.isEmpty { receiveFirstThreeMessages.fulfill() }
+				if content == splitMessage { receiveSplitMessage.fulfill() }
+				if content == lastMessage { receiveLastMessage.fulfill() }
+
+				receiveNext()
+			}
+		}
+
+		client.stateUpdateHandler = { state in
+			print("👨‍💻 client:", state)
+
+			if state == .ready {
+				print("👨‍💻 client: receiving")
+				receiveNext()
+			}
+		}
+
+		client.start(queue: clientQueue)
+
+		wait(for: [receiveFirstThreeMessages, receiveSplitMessage, receiveLastMessage], timeout: 6)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testChunkedInTheMiddle", testChunkedInTheMiddle),
     ]
+}
+
+let messages = ["A", "Be", "", "Split in the middle", "0", "Done"]
+
+func sendChunks(to connection: NWConnection) {
+	let delimitedMessages = messages.joined(separator: "|")
+	let chunk0End = delimitedMessages.range(of: " t")!.lowerBound
+	let chunk1End = delimitedMessages.range(of: "0|")!.lowerBound
+	let firstChunk = delimitedMessages.prefix(upTo: chunk0End)
+	let secondChunk = delimitedMessages[chunk0End..<chunk1End]
+	let thirdChunk = delimitedMessages[chunk1End...]
+
+	print("🖲 server: new connection from", connection.endpoint)
+
+	func send(string: Substring, isComplete: Bool = true) {
+		print("🖲 server: sending \"\(string)\" 🖲")
+		connection.send(content: string.data(using: .utf8)!, isComplete: isComplete, completion: .idempotent)
+	}
+	func reportClientState() {
+		print("🖲 server (client \(connection.endpoint)): state", connection.state)
+	}
+
+	reportClientState()
+
+	connection.viabilityUpdateHandler = { viable in
+		reportClientState()
+		if viable {
+			print("🖲 server: sending")
+			send(string: firstChunk, isComplete: false)
+
+			serverQueue.asyncAfter(deadline: .now() + 2) {
+				send(string: secondChunk)
+			}
+			serverQueue.asyncAfter(deadline: .now() + 4) {
+				send(string: thirdChunk)
+			}
+		}
+	}
+
+	connection.start(queue: serverQueue)
 }
